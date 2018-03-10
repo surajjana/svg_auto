d379e38a94e0576ddc1c38c3c47f95a863f99e3e
Suraj Kumar Jana <surajjana2@gmail.com>
2018-03-10 23:26:34 +0530

    new commit

--- all_svg2pdf.py
+++ all_svg2pdf.py
@@ -1,28 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''
-==============================================================================
-                                all_svg2pdf
 
-    A Python module to convert all .svg files in a root folder (and below) 
-    to .pdf according to the following inkscape options:
-    --without-gui
-    --export-area-drawing
-
-    For more inkscape options see: http://inkscape.org/doc/inkscape-man.html
-
-    Usage: 
-    $python all_svg2pdf.py
-==============================================================================
-'''
 import os
-
-__author__ = "Manolis Georgioudakis"
-__version__ = "Revision: 0.1"
-__date__ = "Date: 2013/09"
-__copyright__ = "Copyright (c) 2013"
-__license__ = "Python"
-
 
 def getPaths():
     """Get root directory and define extension"""
